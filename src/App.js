8b4f079cda61bf9f7ad67082f8d9ee3cc7ee2a8e
aronweston <aron@firststepdigital.com.au>
2021-02-12 16:55:22 +1100

    react router semi-set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import NavBar from './components/NavBar'
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+//set up a pages directory in src
+// create pages directory << Landing.js << Login.js << SignUp.js << Home.js
+// set up browser router
+// set up switch component with nested routes
+// in the routes, define the component ={component}
+// path="/"
 
 function App() {
   return (
