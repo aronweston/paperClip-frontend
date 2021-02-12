503f74514a8bb4380e52b2e7f50e6fb54cb675e5
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-02-12 21:45:03 +1100

    pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to paperClip</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Welcome to paperClip</h1>
+        <Switch>
+          <Route component={Landing} path="/" exact />
+          <Route component={LogIn} path="/login" />
+          <Route component={SignUp} path="/signup" />
+          <Route component={Home} path="/home/:id" />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
