ae048f2c3dac686a242445c6c327432ac028ff11
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-02-13 18:10:04 +1100

    browserRouter-set-up

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,7 @@
-<<<<<<< HEAD:src/App.js
+import NavBar from "./NavBar";
+import Home from "../pages/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Landing from "./pages/Landing";
-import LogIn from "./pages/LogIn";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>Welcome to paperClip</h1>
-        <Switch>
-          <Route component={Landing} path="/" exact />
-          <Route component={LogIn} path="/login" />
-          <Route component={SignUp} path="/signup" />
-          <Route component={Home} path="/home/:id" />
-        </Switch>
-      </div>
-    </BrowserRouter>
-=======
-import NavBar from './NavBar'
-import Home from '../pages/Home'
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import '../app.css'
+import "../app.css";
 
 //set up a pages directory in src
 // create pages directory << Landing.js << Login.js << SignUp.js << Home.js
@@ -34,15 +13,20 @@ import '../app.css'
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route component={Landing} path="/" exact />
+          <Route component={LogIn} path="/login" />
+          <Route component={SignUp} path="/signup" />
+          <Route component={Home} path="/home" />
+        </Switch>
+      </BrowserRouter>
       <NavBar />
 
       <div className="main-container">
         <Home />
       </div>
-
     </div>
->>>>>>> cfd6901a0b285142909af1e5abbe4d501cb1a350:src/components/App.js
   );
 }
 
