1eec8190e8e8a098a7b3942303943e37f9d29256
Andre Anggono <andre.anggono@hotmail.com>
2021-02-18 18:14:15 +1100

    server data git merge issue

--- src/auth/serverData.js
+++ src/auth/serverData.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
-// export const ROOT = "https://paperclip-b.herokuapp.com";
-// export const MESSAGES = "https://paperclip-b.herokuapp.com/messages.json";
-// export const SESSIONS = "https://paperclip-b.herokuapp.com/sessions";
-// export const LOGGED_IN = "https://paperclip-b.herokuapp.com/logged_in";
-// export const LOGOUT = "https://paperclip-b.herokuapp.com/logout";
-// export const REGISTRATIONS = "https://paperclip-b.herokuapp.com/registrations";
-
-export const ROOT = "http://localhost:3000/";
-export const MESSAGES = "http://localhost:3000//messages.json";
-export const SESSIONS = "http://localhost:3000//sessions";
-export const LOGGED_IN = "http://localhost:3000//logged_in";
-export const LOGOUT = "http://localhost:3000//logout";
-export const REGISTRATIONS = "http://localhost:3000/registrations";
-=======
-export const ROOT = 'https://paperclip-b.herokuapp.com/';
-export const MESSAGES = 'https://paperclip-b.herokuapp.com/messages.json';
-export const SESSIONS = 'https://paperclip-b.herokuapp.com/sessions';
-export const LOGGED_IN = 'https://paperclip-b.herokuapp.com/logged_in';
-export const LOGOUT = 'https://paperclip-b.herokuapp.com/logout';
-export const REGISTRATIONS = 'https://paperclip-b.herokuapp.com/registrations';
-export const USERS = 'https://paperclip-b.herokuapp.com/users';
+export const ROOT = "https://paperclip-b.herokuapp.com/";
+export const MESSAGES = "https://paperclip-b.herokuapp.com/messages.json";
+export const SESSIONS = "https://paperclip-b.herokuapp.com/sessions";
+export const LOGGED_IN = "https://paperclip-b.herokuapp.com/logged_in";
+export const LOGOUT = "https://paperclip-b.herokuapp.com/logout";
+export const REGISTRATIONS = "https://paperclip-b.herokuapp.com/registrations";
+export const USERS = "https://paperclip-b.herokuapp.com/users";
 
 // export const ROOT = 'http://localhost:3000/';
 // export const MESSAGES = 'http://localhost:3000//messages.json';
@@ -30,7 +15,7 @@ export const USERS = 'https://paperclip-b.herokuapp.com/users';
 // export const USERS = 'http://localhost:3000/users';
 
 //Don't comment this out
->>>>>>> 97b2ef1e3fe3f94fbf9bdaa2dc213929023cc4fb
+
 export const HEADERS = {
 	"Content-Type": "application/json",
 	Accept: "application/json",
