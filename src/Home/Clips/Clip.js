f4cbb6511f3e220225262eac628657e948a96225
Andre Anggono <andre.anggono@hotmail.com>
2021-02-18 18:10:13 +1100

    Added Clips feature

--- src/Home/Clips/Clip.js
+++ src/Home/Clips/Clip.js
@@ -2,33 +2,27 @@ import React, { useEffect, useState } from "react";
 import RemoveClip from "./RemoveClip";
 
 const Clip = (props) => {
-	if (props && props.clips === null) {
-		return "";
-	} else {
-		// console.log(props);
-		// console.log("heretop", props.clips[0].username);
-		// console.log("here", props.clips[1].text);
-		return (
-			<div>
-				{props.clips.map((c) => {
+	return (
+		<div>
+			{props.clips.map((c) => {
+				return (
 					<div className="clip-list-container">
 						<div className="clip-container">
 							<div className="clip-message-container">
 								<p className="clip-message">{c.text}</p>
 								<p className="clip-info">
 									{c.username} | {c.messaged_at}
-									{/* {props.clips[0].username} | {props.clips[0].messaged_at} */}
 								</p>
 							</div>
 							<div className="clip-button-controls">
 								<RemoveClip />
 							</div>
 						</div>
-					</div>;
-				})}
-			</div>
-		);
-	}
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Clip;
--- src/auth/serverData.js
+++ src/auth/serverData.js
@@ -1,17 +1,17 @@
-export const ROOT = 'https://paperclip-b.herokuapp.com/';
-export const MESSAGES = 'https://paperclip-b.herokuapp.com/messages.json';
-export const SESSIONS = 'https://paperclip-b.herokuapp.com/sessions';
-export const LOGGED_IN = 'https://paperclip-b.herokuapp.com/logged_in';
-export const LOGOUT = 'https://paperclip-b.herokuapp.com/logout';
-export const REGISTRATIONS = 'https://paperclip-b.herokuapp.com/registrations';
+// export const ROOT = "https://paperclip-b.herokuapp.com";
+// export const MESSAGES = "https://paperclip-b.herokuapp.com/messages.json";
+// export const SESSIONS = "https://paperclip-b.herokuapp.com/sessions";
+// export const LOGGED_IN = "https://paperclip-b.herokuapp.com/logged_in";
+// export const LOGOUT = "https://paperclip-b.herokuapp.com/logout";
+// export const REGISTRATIONS = "https://paperclip-b.herokuapp.com/registrations";
 
-// export const ROOT = 'http://localhost:3000/';
-// export const MESSAGES = 'http://localhost:3000//messages.json';
-// export const SESSIONS = 'http://localhost:3000//sessions';
-// export const LOGGED_IN = 'http://localhost:3000//logged_in';
-// export const LOGOUT = 'http://localhost:3000//logout';
-// export const REGISTRATIONS = 'http://localhost:3000/registrations';
+export const ROOT = "http://localhost:3000/";
+export const MESSAGES = "http://localhost:3000//messages.json";
+export const SESSIONS = "http://localhost:3000//sessions";
+export const LOGGED_IN = "http://localhost:3000//logged_in";
+export const LOGOUT = "http://localhost:3000//logout";
+export const REGISTRATIONS = "http://localhost:3000/registrations";
 export const HEADERS = {
-  'Content-Type': 'application/json',
-  Accept: 'application/json',
+	"Content-Type": "application/json",
+	Accept: "application/json",
 };
